99f3a0f80d48a1049bb1a8e9ab96b2e6466c89a9
Nail Fakhrutdinov <75220465+fano2458@users.noreply.github.com>
2023-06-14 17:05:15 +0600

    changed

---
+++ vit/our_vit.py
@@ -0,0 +1,58 @@
+import typing
+import warnings
+import tensorflow as tf
+import typing_extensions as tx
+
+from . import layers, utils
+
+
+ImageSizeArg = typing.Union[typing.Tuple[int, int], int]
+
+
+def interpret_image_size(image_size_arg):
+	"""Process the image_size argument whether a tuple or int."""
+	if isinstance(image_size_arg, int):
+		return (image_size_arg, image_size_arg)
+	if (
+		isinstance(image_size_arg, tuple)
+		and len(image_size_arg) == 2
+		and all(map(lambda v: isinstance(v, int), image_size_arg))
+	):
+		return image_size_arg
+	raise ValueError(
+		f"The image_size argument must be a tuple of 2 integers or a single integer. Received: {image_size_arg}"
+	)
+
+
+def ViT(image_size,patch_size,num_classes,hidden_size,num_layers,
+		num_heads,mlp_dim,dropout,emb_dropout,pool=False):
+	image_size_tuple = interpret_image_size(image_size)
+	assert (image_size_tuple[0] % patch_size == 0) and (
+		image_size_tuple[1] % patch_size == 0
+	), "image_size must be a multiple of patch_size"
+	x = tf.keras.layers.Input(shape=(image_size_tuple[0], image_size_tuple[1], 3))
+	print(x.shape)
+	y = tf.keras.layers.Conv2D(
+		filters=hidden_size,
+		kernel_size=patch_size,
+		strides=patch_size,
+		padding="valid",
+		name="embedding",
+	)(x)
+	y = tf.keras.layers.Reshape((y.shape[1] * y.shape[2], hidden_size))(y)
+	y = layers.ClassToken(name="class_token")(y)
+	y = layers.AddPositionEmbs(name="Transformer/posembed_input")(y)
+	for n in range(num_layers):
+		y, _ = layers.TransformerBlock(
+			num_heads=num_heads,
+			mlp_dim=mlp_dim,
+			dropout=dropout,
+			name=f"Transformer/encoderblock_{n}",)(y)
+	y = tf.keras.layers.LayerNormalization(
+		epsilon=1e-6, name="Transformer/encoder_norm")(y)
+	if pool:
+		y = tf.keras.layers.Lambda(lambda v: tf.reduce_mean(x,axis=1))(y)
+	else:
+		y = tf.keras.layers.Lambda(lambda v: v[:, 0], name="ExtractToken")(y) 
+	y = tf.keras.layers.Dense(num_classes, name="head")(y)
+	return tf.keras.models.Model(inputs=x, outputs=y, name="ViT")
